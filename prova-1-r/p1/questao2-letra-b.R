7c24e1efa310b4a814e23affb2b512b7c78956f9
João Terêncio <jh.terencio@gmail.com>
2024-03-19 23:53:13 -0300

    Questao 3

---
+++ prova-1-r/p1/questao2-letra-b.R
@@ -0,0 +1,16 @@
+# Lendo os dados
+wines <- read.table('http://www.archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data',
+                    sep = ',', col.names = c('Type', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline'))
+
+vinhos_discretizados = wines %>%
+  mutate(across(.cols = !(Type), .fns = function(x) {
+    breaks = quantile(x, probs = seq(0, 1, by = 1/3))
+    labels = c('Baixo', 'Médio', 'Alto')
+    cut(x, breaks, labels, include.lowest = TRUE)
+  }))
+#vinhos_discretizados
+
+cm = categ_mapping('Type')
+
+dw_categ_mapping = cbind(transform(cm, vinhos_discretizados), vinhos_discretizados[-1])
+dw_categ_mapping

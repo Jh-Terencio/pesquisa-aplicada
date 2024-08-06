cadd4ac4a9353869eab2e1305807470b2cbf82e0
Joao-Terencio <jh.terencio@gmail.com>
2024-08-06 02:15:15 -0300

    Code with sub plots

--- final-code.R
+++ final-code.R
@@ -71,10 +71,73 @@ coordinates_geral_ind$cluster <- dbscan_result_ind$cluster
 # Visualizar os clusters
 ggplot(coordinates_geral_ind, aes(x = LONGITUDE, y = LATITUDE, color = as.factor(cluster))) +
   geom_point(alpha = 0.5) +
-  labs(title = "Clusters de Ônibus na cidade do Rio",
+  labs(title = "Clusters de Ônibus na Zona de Chuva 15",
        x = "Longitude",
        y = "Latitude",
        color = "Cluster") +
   theme_minimal()
 
 table(coordinates_geral_ind$cluster)
+
+
+
+# SELECTED RAIN ZONES
+
+selected_rain_zone <- data[data$RAINFALLZONE %in% c(15, 32, 4, 5, 31, 6), ]
+
+sampled_data_rain_zone <- selected_rain_zone[sample(nrow(selected_rain_zone), size = nrow(selected_rain_zone) * 0.07),]
+
+coordinates_geral_ind_rain_zone <- sampled_data_rain_zone[, c("LATITUDE", "LONGITUDE", "Indicator")]
+
+# Removendo colunas NaN
+coordinates_geral_ind_rain_zone <- na.omit(coordinates_geral_ind_rain_zone)
+
+# Normalizando os dados
+coordinates_scaled_ind_rain_zone <- scale(coordinates_geral_ind_rain_zone)
+
+# Executar DBSCAN
+# eps é a distância máxima entre dois pontos para serem considerados vizinhos
+# minPts é o número mínimo de pontos para formar um cluster
+dbscan_result_ind_rain_zone <- dbscan(coordinates_scaled_ind_rain_zone, eps = 0.42, MinPts = 5)
+
+coordinates_geral_ind_rain_zone$cluster <- dbscan_result_ind_rain_zone$cluster
+
+# Visualizar os clusters
+ggplot(coordinates_geral_ind_rain_zone, aes(x = LONGITUDE, y = LATITUDE, color = as.factor(cluster))) +
+  geom_point(alpha = 0.5) +
+  labs(title = "Clusters de Ônibus na Zona de Chuva 15, 32, 4, 5, 31 e 6",
+       x = "Longitude",
+       y = "Latitude",
+       color = "Cluster") +
+  theme_minimal()
+
+
+# ZONA 15
+
+selected_zone_15 <- data[data$RAINFALLZONE %in% c(15), ]
+
+sampled_data_zone_15 <- selected_zone_15[sample(nrow(selected_zone_15), size = nrow(selected_zone_15) * 0.2),]
+
+coordinates_geral_ind_zone_15 <- sampled_data_zone_15[, c("LATITUDE", "LONGITUDE", "Indicator")]
+
+# Removendo colunas NaN
+coordinates_geral_ind_zone_15 <- na.omit(coordinates_geral_ind_zone_15)
+
+# Normalizando os dados
+coordinates_scaled_ind_zone_15 <- scale(coordinates_geral_ind_zone_15)
+
+# Executar DBSCAN
+# eps é a distância máxima entre dois pontos para serem considerados vizinhos
+# minPts é o número mínimo de pontos para formar um cluster
+dbscan_result_ind_zone_15 <- dbscan(coordinates_scaled_ind_zone_15, eps = 0.42, MinPts = 5)
+
+coordinates_geral_ind_zone_15$cluster <- dbscan_result_ind_zone_15$cluster
+
+# Visualizar os clusters
+ggplot(coordinates_geral_ind_zone_15, aes(x = LONGITUDE, y = LATITUDE, color = as.factor(cluster))) +
+  geom_point(alpha = 0.5) +
+  labs(title = "Clusters de Ônibus na Zona de Chuva 15",
+       x = "Longitude",
+       y = "Latitude",
+       color = "Cluster") +
+  theme_minimal()
